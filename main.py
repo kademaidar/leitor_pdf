bedd22298011d3ae5a368e6c0a50b417006b003d
Kadem Aidar <52976640+kademaidar@users.noreply.github.com>
2023-09-27 11:27:09 -0300

    FEAT aplicativo pronto para testes
    App já está:
    - Lendo as respostas de cada campo
    - Preenchendo uma lista para conferencia
    - Imprimindo um df para conferirmos com varios pdfs diferentes

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import streamlit as st
-import functions as f
+import my_functions as mf
 
 
 st.title("AutoPDF !")
 
-lista_pdfs = st.file_uploader(
+pdfs_list = st.file_uploader(
     "Arraste os PDFs", type=["pdf"], accept_multiple_files=True
 )
 
-if len(lista_pdfs) == 0:
+if len(pdfs_list) == 0:
     st.warning("Insira seus PDFs")
 else:
-    text = f.read_pdf(lista_pdfs[0])
+    pdfs_text_list = mf.read_pdfs(pdfs_list)
 
-    st.success(f"Parabens, vc colocou {len(lista_pdfs)} arquivos PDF !")
+    st.success(f"Parabens, vc colocou {len(pdfs_list)} arquivos PDF !")
 
-    st.write(text)
+    for pdf_text in pdfs_text_list:
+        topic_list = mf.split_into_topics(pdf_text)
+
+        answers_list = []
+
+        answers_list.extend(mf.get_topic_3_data(topic_list[3]))
+        answers_list.extend(mf.get_topic_4_data(topic_list[4]))
+        answers_list.extend(mf.get_topic_5_data(topic_list[5]))
+
+        mf.tester(answers_list)
+
+        # answer = mf.funcao(topic_list)
+
+        # st.write(f"ORIGEM: {answer}")
